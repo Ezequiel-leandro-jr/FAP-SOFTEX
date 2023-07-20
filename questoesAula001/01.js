067a3b3e0e02b99f9940aeff3e8c90e62ef933c6
Ezequiel Leandro Jr <55987716+Ezequiel-leandro-jr@users.noreply.github.com>
2023-07-20 17:44:52 +0000

    questões da aula do dia 18/07/23

---
+++ questoesAula001/01.js
@@ -0,0 +1,29 @@
+var minhaVar, soma, fazSol;
+var comida = [];
+
+function divisao(n1,n2){
+    return n1 / n2 ;
+};
+
+minhaVar = 10;
+soma = 15 + 8;
+soma +=1;
+console.log(soma);
+soma *=3;
+console.log(soma);
+fazSol = true;
+comida = ['arroz', 'feijão', 'ovo'];
+console.log(comida[1]);
+console.log(soma === minhaVar);
+console.log(minhaVar <= soma);
+
+
+console.log(divisao(10,2));
+
+
+
+
+
+
+
+

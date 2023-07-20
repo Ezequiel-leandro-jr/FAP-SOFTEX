14298681d149bc3037a4591755048455c3bf0605
Ezequiel Leandro Jr <55987716+Ezequiel-leandro-jr@users.noreply.github.com>
2023-07-20 18:02:07 +0000

    Atividades de Classe

--- aula001.js
+++
@@ -1,9 +0,0 @@
-console.log("********** MENU **********");
-console.log("1 - Listar Clientes");
-console.log("2 - Cadastrar Cliente");
-console.log("3 - Editar Cliente");
-console.log("4 - Remover Cliente");
-console.log("0 - Sair do Sistema");
-console.log("**************************");
-console.log("Opção:___");
-console.log("**************************");

3b6d34838e508788d645b5c8e28a3617dfa693eb
Ezequiel Leandro Jr <55987716+Ezequiel-leandro-jr@users.noreply.github.com>
2023-07-25 17:53:17 +0000

    atividade 6

--- lógicaDeProgramação/atividades/006.js
+++ lógicaDeProgramação/atividades/006.js
@@ -6,12 +6,14 @@ let nome = readline.question('Qual o seu nome?: ');
 console.log('Olá,', nome, '! ');
 let idade = readline.questionInt('Qual a sua idade?: ');
 
-if (idade < 18) {
-    console.log('Você é menor de idade,', nome, '. Não pode dirigir ainda...');
+if(idade < 16){
+    console.log('Você não pode dirigir,', nome,'...');
+} else if(idade >= 16 && idade <= 17){
+    console.log('Você pode dirigir, sob autorização dos pais ou responsáveis.');
 } else {
     let habilitacao = readline.question('Você já possui habilitação? : ');
 
-    if(habilitacao == 'sim' || habilitacao == 'SIM' || habilitacao == 's' || habilitacao == 'S'){
+    if(habilitacao == 'sim' || habilitacao == 'SIM' || habilitacao == 's' || habilitacao == 'S' || habilitacao == 'si' || habilitacao == 'Si' || habilitacao == 'SI' || habilitacao == 'sI'){
         console.log('Você pode dirigir!');
     } else{
         console.log('Infelizmente você não pode dirigir sem habilitação, ', nome, '...');

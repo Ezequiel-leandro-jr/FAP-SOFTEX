6ef8b8ec5080d8d84e204359f72126470107efd4
Ezequiel Leandro Jr <55987716+Ezequiel-leandro-jr@users.noreply.github.com>
2023-07-29 18:23:51 +0000

    create 015.js

---
+++ lógicaDeProgramação/atividades/015.js
@@ -0,0 +1,15 @@
+//habilitando o input de dados através da biblioteca readline-sync
+const readline = require("readline-sync");
+
+do{
+         num = readline.questionFloat('Digite um número par: ');
+
+         if(num % 2 == 0){
+             console.log('\nO número par digitado foi o', num);
+       } else if(num % 2 != 0){
+             console.log("\nO valor é ímpar! Tente novamente.\n");
+       } else {
+             console.log("\nCaractere inválido! Tente novamente.\n");
+        }
+
+} while(num % 2 != 0);

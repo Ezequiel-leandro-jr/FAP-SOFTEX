052fc8d724cfea5060210daea967006b4ba772ca
Ezequiel Leandro Jr <55987716+Ezequiel-leandro-jr@users.noreply.github.com>
2023-08-01 19:07:43 +0000

    atividades classe 01/08

--- lógicaDeProgramação/atividades/020.js
+++ lógicaDeProgramação/atividades/020.js
@@ -7,11 +7,11 @@ encontrado ou não
 const readline = require("readline-sync");
 
 //declaração do array da questão e do op
-const meuArray = [10, 20, 30, 40, 50];
+const numeros = [10, 20, 30, 40, 50];
 let op = false
 
 //função includes para buscar elemento do array
-if(meuArray.includes(25)){
+if(numeros.includes(25)){
     console.log('Valor presente no array.');
 } else{
     console.log('Valor não encontrado.')
@@ -20,8 +20,8 @@ if(meuArray.includes(25)){
 
 /*
 //procurando o valor no array usando a função for
-    for(let i = 0; i < meuArray.length-1; i++){
-        if(meuArray[i] == 25){
+    for(let i = 0; i < numeros.length-1; i++){
+        if(numeros[i] == 25){
             console.log(`valor encontrado no índice ${i}`);
             op = true;
             break;

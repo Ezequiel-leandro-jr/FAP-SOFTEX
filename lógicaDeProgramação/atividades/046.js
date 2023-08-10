67d56bca6e0b0e46b5889b52d86de183cd87151e
Ezequiel Leandro Jr <55987716+Ezequiel-leandro-jr@users.noreply.github.com>
2023-08-10 17:30:54 +0000

    atividade classe 10/08/2023

--- lógicaDeProgramação/atividades/046.js
+++ lógicaDeProgramação/atividades/046.js
@@ -6,6 +6,20 @@ criar uma função onde um parâmetro formal possui um valor padrão
 const readline = require('readline-sync');
 
 //criação da função exibirSaudacao com um parâmetro de valor padrão
-function exibirSaudacao(nome, saudacao = 'Hello,'){
-    console.log(`${saudacao}, ${nome}`);
+function exibirSaudacao(nome, saudacao = 'Hello'){
+    console.log(`${saudacao}, ${nome}!`);
 }
+
+//chamada da função apenas com o primeiro parâmetro
+let n = readline.question('Digite o seu nome: ');
+
+exibirSaudacao(n);
+
+/*
+//chamada da função com os dois parâmetros
+let n = readline.question('Digite o seu nome: ');
+let s = readline.question('Digite uma saudação: ');
+
+exibirSaudacao(n, s);
+
+*/

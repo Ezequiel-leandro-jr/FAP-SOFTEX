8af4eaca3779febe7924f6eee92342ece73073d3
Ezequiel Leandro Jr <55987716+Ezequiel-leandro-jr@users.noreply.github.com>
2023-08-15 17:52:09 +0000

    exemplo recursividade

--- lógicaDeProgramação/atividades/057.js
+++ lógicaDeProgramação/atividades/057.js
@@ -1,6 +1,10 @@
 /* recursividade
 Função que chama a si mesma */
 
+//habilitando a biblioteca readline-sync
+const readline = require('readline-sync');
+
+//criação da função
 function fatorial(n) {
     if(n === 0 || n === 1) {
         return 1; //caso base: fatorial de 0 e 1 = 1
@@ -10,6 +14,6 @@ function fatorial(n) {
 }
 
 //teste da função
-const numero = 5;
+let numero = readline.questionInt('Digite um número inteiro: ');
 console.log(`O fatorial de ${numero} é ${fatorial(numero)}`);
 

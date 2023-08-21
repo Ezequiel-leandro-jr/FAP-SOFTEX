c7a27dcf4657316ab9f67be774bf9cb376a1aa91
Ezequiel Leandro Jr <55987716+Ezequiel-leandro-jr@users.noreply.github.com>
2023-08-21 01:26:49 +0000

    Trabalho_05_calculadora_media

---
+++ 02.JavaScript e Orientação a Objetos/Trabalho/05.calculadora_media.js
@@ -0,0 +1,32 @@
+/*
+Crie um programa que calcule a média entre três notas digitadas pelo usuário.
+*/
+
+//habilitando a biblioteca readline-sync
+const readline = require('readline-sync');
+
+//criando as variáveis globais das três notas e da média
+let nota1, nota2, nota3, media;
+
+//criando a função calc_media
+function calc_media(n1, n2, n3) {
+    let m = (n1 + n2 + n3) / 3;
+    return m;
+}
+
+//comandos para entrada das notas
+console.log('====== CALCULANDO A MÉDIA ======')
+nota1 = readline.questionFloat('NOTA 1: ');
+nota2 = readline.questionFloat('NOTA 2: ');
+nota3 = readline.questionFloat('NOTA 3: ');
+console.log('================================\n\n');
+
+//chamada da função para calcular a média, atribuindo o retorno à variável media
+media = calc_media(nota1, nota2, nota3);
+
+//impressão
+console.log('========== RESULTADO ==========');
+console.log(`NOTA 1: ${nota1}\nNOTA 2: ${nota2}\nNOTA 3: ${nota3}\n\nMÉDIA:  ${media}\n===============================`);
+
+
+
